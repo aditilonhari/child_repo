718bbb1c0beffc3290f25048ebb730d9884997ed
simonxmh <simh@live.ca>
2023-10-13 16:35:34 -0700

    only aws

--- main.tf
+++ main.tf
@@ -1,46 +1,46 @@
-terraform {
-  required_providers {
-    tfe = {
-      version = "~> 0.46.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     tfe = {
+#       version = "~> 0.46.0"
+#     }
+#   }
+# }
 
-provider "tfe" {
-  hostname = var.hostname
-}
+# provider "tfe" {
+#   hostname = var.hostname
+# }
 
-resource "tfe_workspace" "child" {
-  count        = 1
-  organization = var.organization
-  name         = "child-${count.index}-${random_id.child_id.id}"
+# resource "tfe_workspace" "child" {
+#   count        = 1
+#   organization = var.organization
+#   name         = "child-${count.index}-${random_id.child_id.id}"
 
-  lifecycle {
-    postcondition {
-      condition     = self.organization == var.organization 
-      error_message = "org name failed"
-    }
-  }
-}
+#   lifecycle {
+#     postcondition {
+#       condition     = self.organization == var.organization 
+#       error_message = "org name failed"
+#     }
+#   }
+# }
 
-resource "random_id" "child_id" {
-  byte_length = 8
-}
+# resource "random_id" "child_id" {
+#   byte_length = 8
+# }
 
-resource "tfe_variable" "test-var" {
-  key = "test_var"
-  value = random_id.child_id.id
-  category = "env"
-  workspace_id = tfe_workspace.child[0].id
-  description = "This allows the build agent to call back to TFC when executing plans and applies"
+# resource "tfe_variable" "test-var" {
+#   key = "test_var"
+#   value = random_id.child_id.id
+#   category = "env"
+#   workspace_id = tfe_workspace.child[0].id
+#   description = "This allows the build agent to call back to TFC when executing plans and applies"
 
-  lifecycle {
-    postcondition {
-      condition = self.category == "env"
-      error_message = "var name postcondition failed"
-    }
-  }
-}
+#   lifecycle {
+#     postcondition {
+#       condition = self.category == "env"
+#       error_message = "var name postcondition failed"
+#     }
+#   }
+# }
 
 module "empty" {
  source  = "dasmeta/empty/null"
