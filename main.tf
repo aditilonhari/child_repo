145463075fcb26cdd63762233fd6a4b54fb42f90
simonxmh <simh@live.ca>
2023-11-07 08:48:29 -0500

    add

--- main.tf
+++ main.tf
@@ -1,46 +1,46 @@
-# terraform {
-#   required_providers {
-#     tfe = {
-#       version = "~> 0.46.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.46.0"
+    }
+  }
+}
 
-# provider "tfe" {
-#   hostname = var.hostname
-# }
+provider "tfe" {
+  hostname = var.hostname
+}
 
-# resource "tfe_workspace" "child" {
-#   count        = 1
-#   organization = var.organization
-#   name         = "child-${count.index}-${random_id.child_id.id}"
+resource "tfe_workspace" "child" {
+  count        = 1
+  organization = var.organization
+  name         = "child-${count.index}-${random_id.child_id.id}"
 
-#   lifecycle {
-#     postcondition {
-#       condition     = self.organization == var.organization 
-#       error_message = "org name failed"
-#     }
-#   }
-# }
+  lifecycle {
+    postcondition {
+      condition     = self.organization == var.organization 
+      error_message = "org name failed"
+    }
+  }
+}
 
-# resource "random_id" "child_id" {
-#   byte_length = 8
-# }
+resource "random_id" "child_id" {
+  byte_length = 8
+}
 
-# resource "tfe_variable" "test-var" {
-#   key = "test_var"
-#   value = random_id.child_id.id
-#   category = "env"
-#   workspace_id = tfe_workspace.child[0].id
-#   description = "This allows the build agent to call back to TFC when executing plans and applies"
+resource "tfe_variable" "test-var" {
+  key = "test_var"
+  value = random_id.child_id.id
+  category = "env"
+  workspace_id = tfe_workspace.child[0].id
+  description = "This allows the build agent to call back to TFC when executing plans and applies"
 
-#   lifecycle {
-#     postcondition {
-#       condition = self.category == "env"
-#       error_message = "var name postcondition failed"
-#     }
-#   }
-# }
+  lifecycle {
+    postcondition {
+      condition = self.category == "env"
+      error_message = "var name postcondition failed"
+    }
+  }
+}
 
 module "empty" {
  source  = "dasmeta/empty/null"
@@ -63,26 +63,26 @@ module "null_label" {
  version = "0.25.0"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
 
-  required_version = ">= 1.2.0"
-}
+#   required_version = ">= 1.2.0"
+# }
 
-provider "aws" {
-  region  = "us-west-2"
-}
+# provider "aws" {
+#   region  = "us-west-2"
+# }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.nano"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.nano"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
