2307cc60dc43d2e98b1ec96d918f653767a574ac
Simon Huang <simh@live.ca>
2023-12-08 10:00:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,9 +44,65 @@ resource "tfe_variable" "test-var" {
   }
 }
 
+
+################################ MODULES ################################
+
 module "empty" {
  source  = "dasmeta/empty/null"
  version = "1.0.0"
+}
+
+module "null_label" {
+ source = "cloudposse/label/null"
+ version = "0.25.0"
+}
+
+
+module "empty" {
+  source  = "dasmeta/empty/null"
+  version = "1.0.0"
+}
+
+module "cloudposse241" {
+  source = "cloudposse/label/null"
+  version = "0.24.1"
+}
+
+module "eg_prod_bastion" {
+  source = "cloudposse/label/null"
+  version = "0.25.0"
+
+  namespace  = "eg"
+  stage      = "prod"
+  name       = "bastion"
+  attributes = ["public"]
+  delimiter  = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+    "Snapshot"     = "true"
+  }
+}
+
+module "hello" {
+  source  = "app.staging.terraform.io/soak-test-projects_large-2/hello/random"
+  version = "6.0.0"
+  # insert required variables here
+  hellos = {
+    hello        = "this is a hello"
+    second_hello = "this is again a hello"
+  }
+  some_key = "this_is the key again"
+}
+
+module "uuid" {
+  source  = "Invicton-Labs/uuid/random"
+  version = "0.2.0"
+}
+
+module "labels" {
+  source  = "clouddrove/labels/aws"
+  version = "1.3.0"
 }
 
 # priv org
@@ -59,9 +115,4 @@ module "empty" {
 #  }
 #  some_key = "this_is the key"
 #}
-
-module "null_label" {
- source = "cloudposse/label/null"
- version = "0.25.0"
-}
 
